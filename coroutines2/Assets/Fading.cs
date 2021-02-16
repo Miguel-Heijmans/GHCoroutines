eec97392e7b872a749a9d3f953ef758396a755b2
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2021-02-16 13:45:40 +0100

    sfg

---
+++ coroutines2/Assets/Fading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Fade()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            Color c = renderer.material.color;
+            c.a = ft;
+            renderer.material.color = c;
+            yield return null;
+        }
+    }
+
+}
